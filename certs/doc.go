e8da9342a80b20e0139c6b4d3ab887ef0ae81080
Sandor Szücs <sandor.szuecs@zalando.de>
2017-08-04 00:52:58 +0200

    add package godoc  (#84)
    * add godoc and fix generate statement should not be shown in godoc page
    
    * revert aws/godoc

---
+++ certs/doc.go
@@ -0,0 +1,8 @@
+/*
+
+Package certs start a background goroutine to update a
+CertificatesCache of ACM and IAM certificates and an algorithm to find
+the best matching certificate.
+
+*/
+package certs
