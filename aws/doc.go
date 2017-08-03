eae64cb3d148d394f0d7fa1ef5887913c0152ee9
Sandor Szuecs <sandor.szuecs@zalando.de>
2017-08-04 00:16:03 +0200

    revert aws/godoc

--- aws/doc.go
+++ aws/doc.go
@@ -1,10 +1,6 @@
-/*
-Package aws provides some higher level Amazon Web Services
-abstractions for access to common resources.
-
-The exported Adapter can be used to orchestrate and obtain information
-about Elastic Compute Cloud (EC2), EC2 Metadata, Elastic Load
-Balancing (ELB v2, also known as Application Load Balancer - ALB),
-Auto Scaling (ASG) and CloudFormation.
-*/
+// Package aws provides some higher level Amazon Web Services abstractions for access to common resources.
+//
+// The exported Adapter can be used to orchestrate and obtain information about Elastic Compute Cloud (EC2),
+// EC2 Metadata, Elastic Load Balancing (ELB v2, also known as Application Load Balancer - ALB), Auto Scaling (ASG) and
+// CloudFormation.
 package aws
