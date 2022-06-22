3ccfdcfcb7e258627554cdcddc2dad3770a14672
Katyanna Moura <amelie.kn@gmail.com>
2022-06-22 11:33:38 +0200

    Limit ingress support to only networking.k8s.io/v1
    Given the deprecation of extensions/v1beta1 and networking.k8s.io/v1beta1,
    remove support for them.
    
    Signed-off-by: Katyanna Moura <amelie.kn@gmail.com>

--- kubernetes/ingress_test.go
+++ kubernetes/ingress_test.go
@@ -41,33 +41,6 @@ func TestListIngresses(t *testing.T) {
 	}
 }
 
-func TestListIngressesV1(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		f, _ := os.Open("testdata/fixture03.json")
-		defer f.Close()
-		rw.WriteHeader(http.StatusOK)
-		io.Copy(rw, f)
-	}))
-	defer testServer.Close()
-	kubeClient, _ := newSimpleClient(&Config{BaseURL: testServer.URL}, false)
-	ingressClient := &ingressClient{apiVersion: IngressAPIVersionNetworkingV1}
-	want := newList(IngressAPIVersionNetworkingV1,
-		newIngress("fixture01", nil, "", "example.org", "fixture01", IngressAPIVersionNetworkingV1),
-		newIngress("fixture02", map[string]string{ingressClassAnnotation: "skipper"}, "", "skipper.example.org", "fixture02", IngressAPIVersionNetworkingV1),
-		newIngress("fixture03", map[string]string{ingressClassAnnotation: "other"}, "", "other.example.org", "fixture03", IngressAPIVersionNetworkingV1),
-		newIngress("fixture04", nil, "another", "another.example.org", "fixture04", IngressAPIVersionNetworkingV1),
-	)
-	got, err := ingressClient.listIngress(kubeClient)
-	if err != nil {
-		t.Errorf("unexpected error from listIngresses: %v", err)
-	} else {
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("unexpected result from listIngresses. wanted %v, got %v", want, got)
-			t.Errorf("%v", cmp.Diff(want, got))
-		}
-	}
-}
-
 func TestListIngressFailureScenarios(t *testing.T) {
 	for _, test := range []struct {
 		statusCode int
