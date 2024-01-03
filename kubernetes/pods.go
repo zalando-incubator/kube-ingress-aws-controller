15cfbbb94f96b4f41c5170b94849aa489bb67b06
Sandor Szücs <sandor.szuecs@zalando.de>
2024-01-03 15:17:50 +0100

    fix update and pin k8s dependency to v0.24.17
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- kubernetes/pods.go
+++ kubernetes/pods.go
@@ -50,7 +50,7 @@ func (a *Adapter) PodInformer(ctx context.Context, endpointChan chan<- []string)
 	queueEndpoints(&podEndpoints, endpointChan)
 
 	// delta triggered updates
-	_, err = informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		UpdateFunc: func(_, newResource interface{}) {
 			pod, ok := newResource.(*corev1.Pod)
 			if !ok {
@@ -76,9 +76,6 @@ func (a *Adapter) PodInformer(ctx context.Context, endpointChan chan<- []string)
 			}
 		},
 	})
-	if err != nil {
-		return err
-	}
 	<-ctx.Done()
 	return nil
 }
