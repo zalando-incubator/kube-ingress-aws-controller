e343d92167004b5da51d269a8899e2ac13679d9e
Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>
2024-01-03 15:06:04 +0100

    Ingore stacks in `ROLLBACK_IN_PROGRESS` state
    Signed-off-by: Mustafa Abdelrahman <mustafa.abdelrahman@zalando.de>

--- aws/cf.go
+++ aws/cf.go
@@ -505,6 +505,11 @@ func findManagedStacks(svc cloudformationiface.CloudFormationAPI, clusterID, con
 	err := svc.DescribeStacksPages(&cloudformation.DescribeStacksInput{},
 		func(page *cloudformation.DescribeStacksOutput, lastPage bool) bool {
 			for _, s := range page.Stacks {
+
+				if s.StackStatus != nil && *s.StackStatus == cloudformation.StackStatusRollbackInProgress {
+					continue
+				}
+
 				if isManagedStack(s.Tags, clusterID, controllerID) {
 					stacks = append(stacks, mapToManagedStack(s))
 				}
